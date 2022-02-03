285db6c3ee02ebc691674c4bd540c4bf1a1b5ed0
Diego Osvaldo <g11ada18@hackademy.mx>
2022-02-03 18:50:49 -0400

    update list_display

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from api.models import Equipo, Jugador
 
 
-admin.site.register(Equipo)
-admin.site.register(Jugador)
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "city", "created_at", "updated_at"]
+
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "goals", "team"]
+
+
+# admin.site.register(Equipo)
+# admin.site.register(Jugador)
